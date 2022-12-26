5df9a2603f41313ff4c62a383791c3e29f57c700
amr gomaa <amr.gomaa966@gmail.com>
2022-12-27 00:42:50 +0200

    create modified

--- create.sh
+++ create.sh
@@ -1,29 +1,32 @@
 #! /bin/bash
 
+
 function Create_database()
 {
     
-    echo "please enter Database name or choose 5 to Exit"
+    echo "please enter Database name or enter 'exit'"
     read name
     while [[ ! $name =~ ^([a-zA-Z\_])+([a-zA-Z0-9\_])*$ ]]
         do
-         echo "pleas enter a valid name choose 5 to Exit"
-         read name
-        done
-
-    while :
-    do
-        if [ -e $name ];then
-            echo "This name is already taken pleas choose another name choose 5 to Exit"
-            read name
-        else
-        mkdir ./$name
+        echo "pleas enter a valid name or enter 'exit' "
+        read name
+        if [[ $name=='exit' ]];
+        then
         break
         fi
+        done
+
+    while [ -e $name ]  [[ !( $name =~ ^([a-zA-Z\_])+([a-zA-Z0-9\_])*$  "$name" = 'exit') ]]
+    do
+        echo "This name is already taken or invalid name please choose another name or enter 'exit'"
+        read name
     done
+    if [[ $name != 'exit' ]];
+    then
+    mkdir ./$name
+    fi
 
 }
-
 
 
 
