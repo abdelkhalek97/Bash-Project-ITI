40b53187a36fafd2880c8fd85553d7109ec9f907
abdelkhalek97 <ahmed100119@hotmail.com>
2022-12-27 16:25:40 +0200

    fixing regex in select menu in connect&select_menu

--- Select_menu.sh
+++ Select_menu.sh
@@ -4,24 +4,29 @@ function selecting()
 {
     echo "please enter name of table to select from"
     read tableName
-    if [ -e ./$tableName ]
-        then
-            echo "connecting to the table" 
+    while [[ -z "$tableName" ]];
+    do
+        echo "please enter a valid table name"
+        read tableName
+    done
+        if [ -e ./$tableName ]
+            then
+                echo "connecting to the table" 
 
-            select option2 in 'Select all data' 'Select specific colomun' 'Select specific row' 'Back'
-                do
-                    echo $option2
-                    case $option2 in    'Select all data' ) selectAll $tableName ;;
-                                        'Select specific colomun' ) selectColomun $tableName;;
-                                        'Select specific row' ) selectRow $tableName;;
-                                        'Back' ) break;;
-                                    
-                    esac
-                done
+                select option2 in 'Select all data' 'Select specific colomun' 'Select specific row' 'Back'
+                    do
+                        echo $option2
+                        case $option2 in    'Select all data' ) selectAll $tableName ;;
+                                            'Select specific colomun' ) selectColomun $tableName;;
+                                            'Select specific row' ) selectRow $tableName;;
+                                            'Back' ) break;;
 
-            
-    else 
-        echo "No such table, please enter a valid table name"
-    fi
+                        esac
+                    done
+
+
+        else 
+            echo "No such table, please enter a valid table name"
+        fi
 
 }
--- connect.sh
+++ connect.sh
@@ -6,6 +6,11 @@ function c_onn()
 {
     echo "please enter the Database name"
     read Dataname
+    while [[ -z "$Dataname" ]];
+    do
+        echo "please enter a valid Database name"
+        read Dataname
+    done
     if [ -e ./$Dataname ]
         then
             echo "connecting to Database" 
@@ -15,7 +20,7 @@ function c_onn()
                     echo $option1
                     case $option1 in    'Create Table' ) create_table;;
                                         'List Tables' ) listing_tables;;
-                                        'Drop Table' ) break;;
+                                        'Drop Table' ) dropTable;;
                                         'Insert into table' ) insert;;
                                         'Select From Table' ) selecting;;
                                         'Delete From Table'  ) delete;;
@@ -24,13 +29,12 @@ function c_onn()
                                         cd .. 
                                         break
                                         ;;
-                                    
                     esac
                 done
-
-            
     else 
-        echo "not such Database please enter a valid Database name"
+        echo "no such Database please enter a valid Database name"
     fi
 
-}
+}   
+
+
