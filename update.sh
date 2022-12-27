c01703eae32987d3b6b522127c866ebb1aafe9bc
abdelkhalek97 <ahmed100119@hotmail.com>
2022-12-27 21:58:14 +0200

    adding regex for update menu for a null input

--- update.sh
+++ update.sh
@@ -1,38 +1,47 @@
 #! /bin/bash
-
 source updetails.sh
+export LC_COLLATE=C
+shopt -s extglob
 
 function update() {
-    declare -i flag=0
-    read -r -p "please enter table name : " update_table_name
-
-    while [ ! -e $update_table_name ]; do
-        echo "table doesnt exist , try again or exit"
-        read -r update_table_name
-        if [ $update_table_name == 'exit' ]; then
+    flag=0
+    read -r -p "please enter table name : " u_table_name
+    while [[ -z "$u_table_name" ]]; do # user regex to no insert null char
+        read -r -p "please enter valid table name : " u_table_name
+    done
+    while [ ! -e $u_table_name ] || [[ -z "$u_table_name" ]]; do
+        echo "table doesnt exist or invalid input , try again or exit"
+        read -r u_table_name
+        if [[ $u_table_name == 'exit' ]]; then
             flag=1
             break
         fi
     done
-    if [ ! $flag == 1 ]; then
-        read -r -p "please enter column name : " update_column_name
-        flag2=$(check_valid_col_name $update_table_name $update_column_name)
+    if (($flag != 1)); then
+        read -r -p "please enter column name : " u_colomun_name
+        while [[ -z "$u_colomun_name" ]]; do # user regex to no insert null char
+            read -r -p "please enter valid column name : " u_colomun_name
+        done
+        flag2=$(check_valid_col_name $u_table_name $u_colomun_name)
         while [ ! $flag2 ]; do
-            read -r -p "column doesn't exist please enter a valid column name : " update_column_name
-            flag2=$(check_valid_col_name $update_table_name $update_column_name)
-            if [ $update_column_name == 'exit' ]; then
+            read -r -p "column doesn't exist please enter a valid column name : " u_colomun_name
+            flag2=$(check_valid_col_name $u_table_name $u_colomun_name)
+            if [[ $u_colomun_name == 'exit' ]]; then
                 break
             fi
         done
         if [ $flag2 ]; then
-            col_num2=$(get_column_number $update_table_name $update_column_name)
+            col_num2=$(get_column_number $u_table_name $u_colomun_name)
             read -r -p "Please enter value you want to update : " field_update
+            while [[ -z "$field_update" ]]; do # user regex to no insert null char
+                read -r -p "please enter valid value : " field_update
+            done
             declare -a var2
-            var2=$(selection_condition $field_update $col_num2 $update_table_name)
-            col_type=$(head -n1 "$update_table_name.md" | sed $'s/:/ /g' | awk -v i=$col_num2 -F " " '{ print $i }')
+            var2=$(selection_condition $field_update $col_num2 $u_table_name)
+            col_type=$(head -n1 "$u_table_name.md" | sed $'s/:/ /g' | awk -v i=$col_num2 -F " " '{ print $i }')
             echo "$col_type"
 
-            check_datatype $update_table_name $col_num2 $col_type
+            check_datatype $u_table_name $col_num2 $col_type
             declare -i count=0
             for i in ${var2[@]}; do
                 update_from_table $i $col_num2 $update_value
--- updetails.sh
+++ updetails.sh
@@ -1,44 +1,14 @@
 #/usr/bin/bash
 
-function validation() {
-    awk -F':' -v value=$1 '{ 
- if(NR!=1){     
-    if($1==value){ 
-        print(1);
-        }
-}
-} ' "$2"
-}
-
-function check_datatype() {
-    while [ -e $1 ]; do
-        read -p "Enter value to be updated : " update_value
-
-        if (($2 == 1)); then
-            var=$(validation $update_value $1)
-            echo "$var"
-            while [ "$var" == 1 ]; do
-                echo "pk is not unique"
-                read update_value
-                var=$(validation $update_value $1)
-            done
-        fi
-        if [ "$3" = "Integer" ] && [[ $update_value =~ [0-99]+$ ]]; then
-            break
-        elif [ "$3" = "String" ]; then
-            break
-        else
-            echo "type is not matched, type = $3"
-            continue
-        fi
-    done
-}
-
-function selection_condition() {
-    awk -F ':' -v value=$1 -v col_num=$2 '{                    
-        if($col_num == value && NR!=1)
-            print(NR)
-    } ' $3
+function check_valid_col_name() {
+    awk -F : -v value=$2 '{
+        for (i=1; i<NF; i++){
+            if($i == value && NR==1){
+                print(i)
+            }
+            
+            }
+}' $1
 }
 
 function get_column_number() {
@@ -51,15 +21,45 @@ function get_column_number() {
 }' $1
 }
 
-function check_valid_col_name() {
-    awk -F : -v value=$2 '{
-        for (i=1; i<NF; i++){
-            if($i == value && NR==1){
-                print(i)
-            }
-            
-            }
-}' $1
+function selection_condition() {
+    awk -F ':' -v value=$1 -v col_num=$2 '{                    
+        if($col_num == value && NR!=1)
+            print(NR)
+    } ' $3
+}
+
+function validation() {
+    awk -F':' -v value=$1 '{ 
+ if(NR!=1){     
+    if($1==value){ 
+        print(1);
+        }
+}
+} ' "$2"
+}
+
+function check_datatype() {
+    while [ -e $1 ]; do
+        read -p "Enter the updated value : " u_value
+
+        if (($2 == 1)); then
+            var=$(validation $u_value $1)
+            echo "$var"
+            while [ "$var" == 1 ]; do
+                echo "pk is not unique"
+                read u_value
+                var=$(validation $u_value $1)
+            done
+        fi
+        if [ "$3" = "Integer" ] && [[ $u_value =~ [0-99]+$ ]]; then
+            break
+        elif [ "$3" = "String" ]; then
+            break
+        else
+            echo "type is not matched, type = $3"
+            continue
+        fi
+    done
 }
 
 function update_from_table() {
@@ -76,6 +76,6 @@ function update_from_table() {
             else{
                 print;
             }
-        }' $update_table_name) >$update_table_name
-    $(sed -i 's/ /\n/g' $update_table_name)
+        }' $u_table_name) >$u_table_name
+    $(sed -i 's/ /\n/g' $u_table_name)
 }
