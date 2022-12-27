c58f90bb8727f073de324a7a85076958a1cd3a3c
abdelkhalek97 <ahmed100119@hotmail.com>
2022-12-27 23:13:51 +0200

    added connect menu GUI

--- connect.sh
+++ connect.sh
@@ -2,27 +2,60 @@ source connect_menu.sh
 source Select_menu.sh
 source delete1.sh
 source update.sh
-
 function c_onn() {
-    echo "please enter the Database name"
-    read Dataname
+    Dataname=$(whiptail --inputbox "please enter the Database name" 8 39 --title "Connecting to DataBase" 3>&1 1>&2 2>&3)
     while [[ -z "$Dataname" ]]; do
-        echo "please enter a valid Database name"
-        read Dataname
+        Dataname=$(whiptail --inputbox "please enter a valid Database name" 8 39 --title "Connecting to DataBase" 3>&1 1>&2 2>&3)
     done
     if [ -e ./$Dataname ]; then
-        echo "connecting to Database"
+        whiptail --msgbox "Connecting to DataBase" 20 78
         cd $Dataname
-        select option1 in 'Create Table' 'List Tables' 'Drop Table' 'Insert into table' 'Select From Table' 'Delete From Table' 'Update Table' 'Back'; do
-            echo $option1
-            case $option1 in 'Create Table') create_table ;;
-            'List Tables') listing_tables ;;
-            'Drop Table') dropTable ;;
-            'Insert into table') insert ;;
-            'Select From Table') selecting ;;
-            'Delete From Table') delete ;;
-            'Update Table') update ;;
-            'Back')
+
+        while [ 1 ]; do
+
+            CHOICE=$(
+                whiptail --title "Operative Systems" --menu "Make your choice" 16 100 9 \
+                    "1)" "Create Table" \
+                    "2)" "List Tables" \
+                    "3)" "Drop Table" \
+                    "4)" "Insert into table" \
+                    "5)" "Select From Table" \
+                    "6)" "Delete From Table" \
+                    "7)" "Update Table" \
+                    "8)" "Back" 3>&2 2>&1 1>&3
+            )
+
+            case $CHOICE in
+            "1)")
+                create_table
+                ;;
+
+            "2)")
+                listing_tables
+                ;;
+
+            "3)")
+                insert
+                ;;
+
+            "4)")
+                dropTable
+                ;;
+
+            "5)")
+                selecting
+                ;;
+
+            "6)")
+                delete
+                ;;
+
+            "7)")
+                update
+                ;;
+
+            "8)")
+                whiptail --msgbox "returning to main menu..." 20 78
                 cd ..
                 break
                 ;;
